541aa85a3eb992aa2301102e2bfa81333e5d8ee3
Shravani <shravanichendwankar@gmail.com>
2025-12-21 14:37:30 +0530

    delete

--- backend/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { register } from '../controllers/userController.js';
-
-const userRouter = express.Router();
-
-userRouter.post('/register', register)
-
-export default userRouter;
