f812936fdb47f8d3ea0d9e3543cde58728df01e7
ShravaniCoder <shravanichendwankar@gmail.com>
2026-02-03 16:52:58 +0530

    solved error

--- frontend-web/src/components/SubPlatinumForm.jsx
+++ frontend-web/src/components/SubPlatinumForm.jsx
@@ -41,7 +41,23 @@ const PlatinumsubForm = () => {
   const [membershipId, setMembershipId] = useState("");
   const [loadingOrder, setLoadingOrder] = useState(false);
   const [showSuccessPopper, setShowSuccessPopper] = useState(false);
-const { width, height } = useWindowSize();
+const [windowSize, setWindowSize] = useState({
+  width: window.innerWidth,
+  height: window.innerHeight,
+});
+
+useEffect(() => {
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  window.addEventListener("resize", handleResize);
+  return () => window.removeEventListener("resize", handleResize);
+}, []);
+
 
 
   // DELIVERY SLOT STATE
@@ -198,7 +214,7 @@ const handlePayment = async () => {
 //      alert("Something went wrong while placing order");
 //    }
 //   };
-  
+
 
 
 useEffect(() => {
@@ -599,9 +615,9 @@ useEffect(() => {
                {step === 5 && showSuccessPopper && (
   <>
     {/* 🎊 CONFETTI */}
-    <Confetti
-      width={width}
-      height={height}
+   <Confetti
+  width={windowSize.width}
+  height={windowSize.height}
       numberOfPieces={300}
       gravity={0.25}
       recycle={false}
